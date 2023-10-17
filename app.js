638bc8b3c6afd8e9986f2a4618a5ff1819faae28
Addarshkumar <addarsh.kumar@kalvium.community>
2023-10-17 21:31:11 +0530

    todoapp

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 
 
-var inputText=document.getElementById("input")
-var submitButton =document.getElementById("button")
-var toDoList=document.getElementById("todolist")
+var inputText = document.getElementById("input")
+var submitButton = document.getElementById("button")
+var toDoList = document.getElementById("todolist")
 
-var toDoArray=[];
-submitButton.addEventListener("click",addItemToArr);
+var toDoArray=JSON.parse(localStorage.getItem("toDoArray")) ||[];
+display()
 
-inputText.addEventListener("keypress",(event)=>{
+var toDoArray = [];
+submitButton.addEventListener("click", addItemToArr);
+
+inputText.addEventListener("keypress", (event) => {
     //extra :  event.target.value is same inputText.value
-    if(event.key=="Enter"){
+    if (event.key == "Enter") {
         addItemToArr()
     }
 })
-function addItemToArr(){
-    if(inputText.value!=""){
-    toDoArray.push(inputText.value);
-    
+function addItemToArr() {
+    if (inputText.value != "") {
+        toDoArray.push(inputText.value);
+        localStorage.setItem("toDoArray",JSON.stringify(toDoArray))
 
+    }
+    inputText.value = ""
+    display();
 }
-inputText.value=""
-display();
-}
 
-function display(){
-   toDoList.innerHTML=""
+function display() {
+    toDoList.innerHTML = ""
 
-   toDoArray.map((curr,i)=>{
-    var listItem=`<li id="item ${i}">
+    toDoArray.map((curr, i) => {
+    var listItem = `<li id="item${i}">
     <div>${curr}</div>
     
   <div>
@@ -38,23 +41,52 @@ function display(){
 
 </li>`
 
-   toDoList.innerHTML+=listItem
-   })
-    
+        toDoList.innerHTML += listItem
+    })
+    localStorage.setItem("toDoArray",JSON.stringify(toDoArray))
+
 }
 
-function deleteItem(i){
-   toDoArray.splice(i,1)
-   display()
-}
-
-function addItem(i){
-    var newValue=prompt("Enter a new value")
-    toDoArray.splice(i,1,newValue)
+function deleteItem(i) {
+    toDoArray.splice(i, 1)
     display()
+    localStorage.setItem("toDoArray",JSON.stringify(toDoArray))
 }
 
-document.getElementById("reset").addEventListener("click",()=>{
-    toDoArray=[]
+function addItem(i) {
+    var newValue = prompt("Enter a new value")
+    toDoArray.splice(i, 1, newValue)
     display()
+    localStorage.setItem("toDoArray",JSON.stringify(toDoArray))
+}
+
+document.getElementById("reset").addEventListener("click", () => {
+    toDoArray = []
+    display()
+    localStorage.setItem("toDoArray",JSON.stringify(toDoArray))
 })
+
+//  //local storage 
+// var cartArr = ["Book","Pen","Eraser"]
+// localStorage.setItem("cart",JSON.stringify(cartArr))
+
+// cartArr.push("scale")
+// localStorage.setItem("cart",JSON.stringify(cartArr))
+// console.log(cartArr)
+
+// var tempArr = JSON.parse(localStorage.getItem)
+// console.log(tempArr)
+
+// //sessionstorage
+// var cartArr = ["Book","Pen","Eraser"]
+// sessionStorage.setItem("cart",JSON.stringify(cartArr))
+
+// cartArr.push("scale")
+// sessionStorage.setItem("cart",JSON.stringify(cartArr))
+// console.log(cartArr)
+
+// var tempArr = JSON.parse(sessionStorage.getItem)
+// console.log(tempArr)
+ 
+var arr =JSON.parse(localStorage.getItem("toDoArray"))
+
